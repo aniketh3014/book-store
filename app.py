b8f4a09e3533eb83887b45b08ad1dba2100654d3
Aniket <ghoshaniket1000@gmail.com>
2023-07-27 22:31:44 +0530

    i dont know what is wrong

--- app.py
+++ app.py
@@ -13,21 +13,27 @@ Session(app)
 
 @app.route("/")
 def index():
-    books=cur.execute("SELECT * FROM books")
-    db.commit()
+    books=cur.execute("SELECT * FROM books").fetchall()
     return render_template("books.html", books=books)
 
-@app.route("/cart")
+@app.route("/cart", methods=["POST", "GET"])
 def cart():
     if "cart" not in session:
         session["cart"] = []
+
+    print("Cart Session:", session["cart"])  # Add this line for debugging
+
     if request.method == "POST":
         id = request.form.get("id")
         if id:
             session["cart"].append(id)
+            print("Added Book ID to Cart:", id)  # Add this line for debugging
         return redirect("/cart")
-    for ID in session["cart"]:
-        books = cur.execute("SELECT * FROM books WHERE id IN (?)",ID)
+    
+    books = cur.execute("SELECT * FROM books WHERE id IN ({})".format(','.join(session["cart"]))).fetchall()
+
+    print("Cart Books:", books)  # Add this line for debugging
+
     return render_template("cart.html", books=books)
     
 
