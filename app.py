79189675c7ff031559fa625b22f13bbaa283d5f2
Aniket <ghoshaniket1000@gmail.com>
2023-07-27 21:21:03 +0530

    added cart.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
 import sqlite3
 
@@ -14,10 +14,20 @@ Session(app)
 @app.route("/")
 def index():
     books=cur.execute("SELECT * FROM books")
+    db.commit()
     return render_template("books.html", books=books)
 
 @app.route("/cart")
 def cart():
     if "cart" not in session:
         session["cart"] = []
-        
+    if request.method == "POST":
+        id = request.form.get("id")
+        if id:
+            session["cart"].append(id)
+        return redirect("/cart")
+    for ID in session["cart"]:
+        books = cur.execute("SELECT * FROM books WHERE id IN (?)",ID)
+    return render_template("cart.html", books=books)
+    
+
