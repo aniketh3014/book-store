c70c83e2fddf55389b22eae27bda63e1d885b983
Aniket <ghoshaniket1000@gmail.com>
2023-07-30 08:59:18 +0530

    used cs50 to make the code simple

--- app.py
+++ app.py
@@ -1,39 +1,38 @@
-from flask import Flask, render_template, request, session, redirect
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-import sqlite3
 
-app =Flask(__name__)
+app = Flask(__name__)
 
-db = sqlite3.connect("store.db", check_same_thread=False)
-cur = db.cursor()
-#configure session
+# Connect to database
+db = SQL("sqlite:///store.db")
+
+# Configure session
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route("/")
 def index():
-    books=cur.execute("SELECT * FROM books").fetchall()
+    books = db.execute("SELECT * FROM books")
     return render_template("books.html", books=books)
 
-@app.route("/cart", methods=["POST", "GET"])
+
+@app.route("/cart", methods=["GET", "POST"])
 def cart():
+
+    # Ensure cart exists
     if "cart" not in session:
         session["cart"] = []
 
-    print("Cart Session:", session["cart"])  # Add this line for debugging
-
+    # POST
     if request.method == "POST":
         id = request.form.get("id")
         if id:
             session["cart"].append(id)
-            print("Added Book ID to Cart:", id)  # Add this line for debugging
         return redirect("/cart")
-    
-    books = cur.execute("SELECT * FROM books WHERE id IN ({})".format(','.join(session["cart"]))).fetchall()
 
-    print("Cart Books:", books)  # Add this line for debugging
-
+    # GET
+    books = db.execute("SELECT * FROM books WHERE id IN (?)", session["cart"])
     return render_template("cart.html", books=books)
-    
-
